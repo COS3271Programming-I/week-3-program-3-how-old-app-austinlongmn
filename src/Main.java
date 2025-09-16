175b9ab739bd82ceef1db96cff8ef37e2725ded0
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-09-16 18:13:43 -0500

    Wrote program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+public class Main {
+
+  // Initialized with STDIN
+  static Scanner userinput = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    // Changed to integer instead of string for arithmetic
+    int s;
+
+    // Added trailing space for nicer user input
+    System.out.print("How old are you? ");
+
+    // Changed to `nextInt` to accommodate line 10
+    s = userinput.nextInt();
+
+    // Flush newline
+    userinput.nextLine();
+
+    // Removed self-assignment to avoid messing up calculation on line 28
+    // If I would have left it, I would have needed to change `s + 5 = s` to
+    // `s = s + 5` because `s + 5` is an rvalue, not an lvalue.
+    System.out.println("In 5 years you will be " + (s + 5) + " years old.");
+
+    // Added parenthesis for grouping and fixed faulty logic (changed or to and)
+    System.out.println(
+      "It is " + ((s < 20) && (s > 12)) + " that you are a teenager."
+    );
+
+    System.out.println("I am such a smart computer.");
+  }
 }
